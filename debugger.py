61420a1f4f7a1e6b847d76b1fce6aacf0e48d997
Jonas Schlick <jonasfschlick@gmail.com>
2023-08-13 15:33:25 +0200

    fixed

--- debugger.py
+++ debugger.py
@@ -16,3 +16,38 @@ def perform_shallow_analysis(script):
         if not ok:
             errors.append(i)
     return errors
+
+class result():
+    def __init__(self, status, data) -> None:
+        self.status = status
+        self.data = data
+def perform_analysis(script:str):
+    varspace = [0]*65536
+    imgVars = []
+    pointers = []
+    lines = script.split("\n")
+    for i, line in enumerate(lines):
+        lines[i] = line.split(" ")
+    errors = perform_shallow_analysis(script)
+    if errors:
+        print("FATAL: Cannot perform deep analysis, because syntax is incorrect")
+        return result("ERROR", errors)
+    warnings = []
+    init_blocks = []
+    tick_blocks = []
+    for i, line in enumerate(lines):
+        if line[0] == "@":
+            if line[1] == "on_init":
+                init_blocks.append(i)
+            if line[1] == "on_tick":
+                tick_blocks.append(i)
+    for i in range (1, len(init_blocks)):
+        warnings.append({
+            "line": i,
+            "message": "Duplicate init block. "
+        })
+    
+    if warnings:
+        print("WARNING possible errors found")
+        return result("WARNING",warnings)
+    return result("OK", [])
